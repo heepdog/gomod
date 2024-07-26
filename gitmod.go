489e8e25784e48fde1b6a8f5cb3cd52807831b3a
heepdog <heppners@gmail.com>
2024-07-26 10:44:12 -0500

    added main

--- gitmod.go
+++ gitmod.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func HelloGomod() string {
 
-    message := fmt.Sprintf("Hello from newest gomod v 3.0")
+    message := fmt.Sprintf("Hello from newest gomod v 3.0 main")
 
     return message
 }
